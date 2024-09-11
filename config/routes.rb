324b81679dda928b6b9c9a16a326b4630457e0cc
Neil Gautam <neilgautam.24@tamu.edu>
2024-09-11 13:06:14 -0500

    part 3 is complete

--- config/routes.rb
+++ config/routes.rb
@@ -12,3 +12,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+Rails.application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
+end
